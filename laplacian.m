7fbea5e75e741c54075376d37efe0335fd9870aa
nsbalbi <60553434+nsbalbi@users.noreply.github.com>
2020-05-12 12:37:46 -0400

    Update laplacian.m

--- laplacian.m
+++ laplacian.m
@@ -6,19 +6,17 @@ arrOut = zeros(len,len);
 for i = 1:len
    for j = 1:len
       lInd = i-1;
+      rInd = i+1;
       if lInd < 1
          lInd = len; 
-      end
-      rInd = i+1;
-      if rInd > len
+      elseif rInd > len
          rInd = 1; 
       end
       uInd = j-1;
+      dInd = j+1;
       if uInd < 1
          uInd = len; 
-      end
-      dInd = j+1;
-      if dInd > len
+      elseif dInd > len
          dInd = 1; 
       end
       arrOut(i,j) = -1*arrIn(i,j)+0.2*(arrIn(lInd,j)+arrIn(rInd,j)+arrIn(i,uInd)+arrIn(i,dInd))+0.05*(arrIn(lInd,uInd)+arrIn(rInd,uInd)+arrIn(lInd,dInd)+arrIn(rInd,dInd));
